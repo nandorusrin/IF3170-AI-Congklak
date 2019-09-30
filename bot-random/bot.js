e199cd42b118c82025c89555f9c610d6f67aaf7c
nandorusrin <nando_rusrin@yahoo.com>
2019-09-30 13:31:05 +0700

    add randomMove bot

---
+++ bot-random/bot.js
@@ -0,0 +1,20 @@
+let states = {
+	'player1': {
+		'home': 0,
+		'villages': [7, 7, 7, 7, 7, 7, 7]
+	}, 
+	'player2': {
+		'home': 0,
+		'villages': [7, 7, 7, 7, 7, 7, 7]
+	}
+}
+
+function randomMove(states) {
+	
+	states.player1.villages[i];
+	Math.floor(Math.random() * 7);
+
+
+}
+
+
